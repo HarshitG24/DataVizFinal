9b7e31ae010f4902ee19fef016f5d29f462f4a99
Mihir <mesiamihir@gmail.com>
2023-04-11 11:35:05 -0700

    viz added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import "./App.css";
-import Notebook from "./harshit_viz/Notebook";
-import TreeMap from "./harshit_viz/TreeMap";
-// import TreeMap from "./harshit_viz/TreeMap";
-import Trends from "./harshit_viz/Trends";
 import Causes from "./mihir_viz/Causes";
 import Method from "./mihir_viz/Method"
 
